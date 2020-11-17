51bd235ce96270b73703bb5512c9cf4416545c9a
Matteo <matteo.buldrini94@gmail.com>
2020-11-17 22:50:49 +0100

    Create app.js

---
+++ app.js
@@ -0,0 +1,30 @@
+const hamburger = document.querySelector('.header .nav-bar .nav-list .hamburger');
+const mobile_menu = document.querySelector('.header .nav-bar .nav-list ul');
+const menu_item = document.querySelectorAll('.header .nav-bar .nav-list ul li a');
+const header = document.querySelector('.header.container');
+
+/*Show/Hide menu while clicking*/
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    mobile_menu.classList.toggle('active');
+})
+
+/*Add background to header when scrolling down*/
+document.addEventListener('scroll', () => {
+    var scroll_position = window.scrollY;
+    if (scroll_position >= 0) {
+        /*Color of top bar*/
+        /*Blue: #0d3666 - Dark grey: #29323c - Light grey: #3b4753 - Black: #0d1013 */
+        header.style.backgroundColor = '#0d1013';
+    } else {
+        header.style.backgroundColor = 'transparent';
+    }
+});
+
+/*Automatically close the hamburger menu when selecting a section*/
+menu_item.forEach(item=>{
+    item.addEventListener('click', () => {
+        hamburger.classList.toggle('active');
+        mobile_menu.classList.toggle('active');
+    })
+})
